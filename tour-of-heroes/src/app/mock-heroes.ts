175cef85921e3199369cf553da0833084fec44a5
LucasNomi <lucas04.dev@gmail.com>
2023-05-20 13:28:00 -0300

    feat: tour of heroes e3

---
+++ tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    {id: 12, name: "Luffy"},
+    {id: 13, name: "Zoro"},
+    {id: 14, name: "Nami"},
+    {id: 15, name: "Usopp"},
+    {id: 16, name: "Sanji"},
+    {id: 17, name: "Chopper"},
+    {id: 18, name: "Robin"},
+    {id: 19, name: "Franky"},
+    {id: 20, name: "Jinbe"},
+    {id: 21, name: "Vivi"},
+]
